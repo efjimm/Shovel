a521cad96ddfc53e4b6dc86cfa3ab6f919870c94
evan <evanbonner99@gmail.com>
2023-04-02 03:31:19 +0100

    Update to zig master

--- build.zig
+++ build.zig
@@ -1,54 +1,81 @@
 const std = @import("std");
-const Builder = std.build.Builder;
+const Build = std.Build;
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const tests = b.addTest("test_main.zig");
-    tests.setTarget(target);
-    tests.setBuildMode(mode);
+    const tests = b.addTest(.{
+    	.root_source_file = .{ .path = "test_main.zig" },
+    	.target = target,
+    	.optimize = optimize,
+    });
     const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&tests.step);
 
     {
-        const exe = b.addExecutable("menu", "example/menu.zig");
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
-        exe.addPackagePath("spoon", "import.zig");
+        const exe = b.addExecutable(.{
+        	.name = "menu",
+        	.root_source_file = .{ .path = "example/menu.zig" },
+        	.target = target,
+        	.optimize = optimize,
+        });
+        exe.addAnonymousModule("spoon", .{
+        	.source_file = .{ .path = "import.zig" },
+        });
         exe.install();
     }
 
     {
-        const exe = b.addExecutable("menu-libc", "example/menu.zig");
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
-        exe.addPackagePath("spoon", "import.zig");
+        const exe = b.addExecutable(.{
+        	.name = "menu-libc",
+        	.root_source_file = .{ .path = "example/menu.zig" },
+        	.target = target,
+        	.optimize = optimize,
+        });
         exe.linkLibC();
+        exe.addAnonymousModule("spoon", .{
+        	.source_file = .{ .path = "import.zig" },
+        });
         exe.install();
     }
 
     {
-        const exe = b.addExecutable("input-demo", "example/input-demo.zig");
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
-        exe.addPackagePath("spoon", "import.zig");
+		const exe = b.addExecutable(.{
+			.name = "input-demo",
+			.root_source_file = .{ .path = "example/input-demo.zig" },
+			.target = target,
+			.optimize = optimize,
+		});
+        exe.addAnonymousModule("spoon", .{
+        	.source_file = .{ .path = "import.zig" },
+        });
         exe.install();
     }
 
     {
-        const exe = b.addExecutable("colours", "example/colours.zig");
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
-        exe.addPackagePath("spoon", "import.zig");
+    	const exe = b.addExecutable(.{
+    		.name = "colours",
+    		.root_source_file = .{ .path = "example/colours.zig" },
+    		.target = target,
+    		.optimize = optimize,
+    	});
+        exe.addAnonymousModule("spoon", .{
+        	.source_file = .{ .path = "import.zig" },
+        });
         exe.install();
     }
 
     {
-        const exe = b.addExecutable("table-256-colours", "example/table-256-colours.zig");
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
-        exe.addPackagePath("spoon", "import.zig");
+        const exe = b.addExecutable(.{
+        	.name = "table-256-colours",
+        	.root_source_file = .{ .path = "example/table-256-colours.zig" },
+        	.target = target,
+        	.optimize = optimize,
+        });
+        exe.addAnonymousModule("spoon", .{
+        	.source_file = .{ .path = "import.zig" },
+        });
         exe.install();
     }
 }
