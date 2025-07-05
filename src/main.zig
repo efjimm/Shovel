1dcf60f816ac971078d6350e02de824b3e2eb508
evan <evanbonner99@gmail.com>
2025-07-06 06:54:21 +0900

    Add writeTruncating function
    Faster alternative to TerminalCellWriter when the entire string is known
    at once.

--- src/main.zig
+++ src/main.zig
@@ -51,8 +51,53 @@ pub fn graphemeWidth(bytes: []const u8, mode: GraphemeClusteringMode) u32 {
                 width += @import("util.zig").wcWidth(cp);
             return width;
         },
-        .grapheme => return @intCast(zg.display_width.strWidth(bytes)),
+        .grapheme => return @intCast(zg.display_width.strWidth(bytes, .{}).width),
     }
+}
+
+pub const TextAlignment = enum { left, right, center };
+
+pub fn getLeftRightPadding(pad: u32, alignment: TextAlignment) [2]u32 {
+    return switch (alignment) {
+        .left => .{ 0, pad },
+        .right => .{ pad, 0 },
+        .center => .{ pad / 2, pad - (pad / 2) },
+    };
+}
+
+/// Write a single string, truncating above `max_width`. For formatted printing or incremental
+/// writes see `terminal_cell_writer`.
+pub fn writeTruncating(
+    str: []const u8,
+    max_width: u32,
+    alignment: TextAlignment,
+    writer: anytype,
+) !void {
+    const res = zg.display_width.strWidth(str, .{ .max_width = max_width });
+    const width: u32 = @intCast(res.width);
+    if (res.len == str.len) {
+        const pad = max_width - width;
+        const left, const right = getLeftRightPadding(pad, alignment);
+
+        try writer.writeByteNTimes(' ', left);
+        try writer.writeAll(str);
+        try writer.writeByteNTimes(' ', right);
+        return;
+    }
+
+    const truncated = str[0..res.len];
+    if (width == max_width) {
+        var iter = zg.graphemes.reverseIterator(truncated);
+        const g = iter.prev().?;
+        try writer.print("{s}…", .{truncated[0..g.offset]});
+        return;
+    }
+
+    const pad = max_width - width - 1;
+    const left, const right = getLeftRightPadding(pad, alignment);
+    try writer.writeByteNTimes(' ', left);
+    try writer.print("{s}…", .{truncated});
+    try writer.writeByteNTimes(' ', right);
 }
 
 test {
